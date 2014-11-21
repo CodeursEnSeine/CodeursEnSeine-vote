548770f2428674f72411b75380dc584c8c909ea8
youenchene <youen.chene@gadz.org>
2014-11-21 23:06:59 +0100

    clean target

--- target/classes/db.sql
+++
@@ -1,9 +0,0 @@
-create DATABASE cesvote;
-
-CREATE TABLE IF NOT EXISTS `vote` (
-  `ID` bigint(20) NOT NULL auto_increment,
-  `VOTEDATETIME` datetime NOT NULL,
-  `TALKID` varchar(255) NOT NULL,
-  `VOTE` int(11)  NOT NULL,
-  PRIMARY KEY  (`ID`)
-) ENGINE=InnoDb;
